f83c6adf5b6a7e57bb180d071b1f8bc1ecb46524
PABLO <fernandez.pablo44@gmail.com>
2020-10-11 19:28:46 -0300

    set connection variables to heroku

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "crudphp";
+    $host = "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "t92ehk92dp4sdira";
+    $pass = "x4c57qt0dmamvl72";
+    $db = "kjl3o217yheevyn8";
 
     $conn = new mysqli($host,$user,$pass,$db);
 
